0d9d864ab72c033f5b7dc1ca5e5efb4b61b3e827
Erni <Erni@Erni-PC>
2013-02-21 11:02:50 -0800

    trying to get hibernate to work. failing.

--- src/JAXB/unmarshallers/AuthorJAXBUnMarshaller.java
+++ src/JAXB/unmarshallers/AuthorJAXBUnMarshaller.java
@@ -5,14 +5,70 @@ import java.util.List;
 import javax.xml.bind.*;
 import jaxb.generated.author.AuthorRoot;
 import jaxb.generated.author.AuthorType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 // Adapted from Pro XML Development with Java Technology
 // by Ajay Vohra and Deepak Vohra
 // Apress, 2006
+@Entity(name = "Author")
 public class AuthorJAXBUnMarshaller
 {
+   private long id;
+   private String firstName;
+   private String lastName;
+
+   public AuthorJAXBUnMarshaller(long id, String firstName, String lastName)
+   {
+      this.id = id;
+      this.firstName = firstName;
+      this.lastName = lastName;
+   }
+
+   public AuthorJAXBUnMarshaller()
+   {
+   }
+
+   @Id
+   @Column(name = "id")
+   public long getId()
+   {
+      return id;
+   }
+
+   public void setId(long id)
+   {
+      this.id = id;
+   }
+
+   @Column(name = "first_name")
+   public String getFirstName()
+   {
+      return firstName;
+   }
+
+   public void setFirstName(String firstName)
+   {
+      this.firstName = firstName;
+   }
+
+   @Column(name = "last_name")
+   public String getLastName()
+   {
+      return lastName;
+   }
+
+   public void setLastName(String lastName)
+   {
+      this.lastName = lastName;
+   }
+
    public void unMarshall(File xmlDocument)
    {
+
       try
       {
          JAXBContext jaxbContext =
@@ -21,18 +77,30 @@ public class AuthorJAXBUnMarshaller
 
          JAXBElement<AuthorRoot> catalogElement =
                  (JAXBElement<AuthorRoot>) unMarshaller.unmarshal(xmlDocument);
-         AuthorRoot catalog = catalogElement.getValue();
-         List<AuthorType> journalList = catalog.getAuthorInfo();
+         AuthorRoot author = catalogElement.getValue();
+         List<AuthorType> authorList = author.getAuthorInfo();
 
-         for (AuthorType journal : journalList)
+
+         Session session = HibernateContext.getSession();
+
+
+         Transaction tx = session.beginTransaction();
          {
-            System.out.printf("\nAUTHOR id = '%d'\n",
-                    journal.getAuthorId());
-            System.out.printf("        firstName = '%s'\n",
-                    journal.getFirstName());
-            System.out.printf("        lastName = '%s'\n",
-                    journal.getLastName());
+            for (AuthorType auth : authorList)
+            {
+               session.save(new AuthorJAXBUnMarshaller(auth.getAuthorId(), auth.getFirstName(), auth.getLastName()));
+            }
          }
+//            System.out.printf("\nAUTHOR id = '%d'\n",
+//                    journal.getAuthorId());
+//            System.out.printf("        firstName = '%s'\n",
+//                    journal.getFirstName());
+//            System.out.printf("        lastName = '%s'\n",
+//                    journal.getLastName());
+         tx.commit();
+         session.close();
+         System.out.println("Author table loaded.");
+
       }
       catch (JAXBException ex)
       {
