56103ad603d8f025e626716c83fcc055380e5256
Erni <Erni@Erni-PC>
2013-02-20 20:33:38 -0800

    commented out

--- src/JAXB/JAXBMarshaller.java
+++ src/JAXB/JAXBMarshaller.java
@@ -18,39 +18,39 @@ public class JAXBMarshaller
 			Marshaller marshaller = jaxbContext.createMarshaller();
 			ObjectFactory factory = new ObjectFactory();
 
-			CatalogType catalog = factory.createCatalogType();
-			List<JournalType> journalList = catalog.getJournal();
-
-			JournalType journal = factory.createJournalType();
-			List<ArticleType> articleList = journal.getArticle();
-
-			journalList.add(journal);
-
-			ArticleType article = factory.createArticleType();
-			article.setTitle("Service Oriented Architecture Frameworks");
-			article.setAuthor("Naveen Balani");
-			article.setLevel("Intermediate");
-			article.setDate("January-2004");
-			articleList.add(article);
-
-			article = factory.createArticleType();
-			article.setTitle("Advance DAO Programming");
-			article.setAuthor("Sean Sullivan");
-			article.setLevel("Advanced");
-			article.setDate("October-2003");
-			articleList.add(article);
-
-			article = factory.createArticleType();
-			article.setTitle("Best Practices in EJB Exception Handling");
-			article.setAuthor("Srikanth Shenoy");
-			article.setLevel("Advanced");
-			article.setDate("May-2002");
-			articleList.add(article);
-
-			JAXBElement<CatalogType> catalogElement =
-                    factory.createCatalog(catalog);
-			marshaller.setProperty("jaxb.formatted.output", Boolean.TRUE);
-			marshaller.marshal(catalogElement, System.out);
+//			CatalogType catalog = factory.createCatalogType();
+//			List<JournalType> journalList = catalog.getJournal();
+//
+//			JournalType journal = factory.createJournalType();
+//			List<ArticleType> articleList = journal.getArticle();
+//
+//			journalList.add(journal);
+//
+//			ArticleType article = factory.createArticleType();
+//			article.setTitle("Service Oriented Architecture Frameworks");
+//			article.setAuthor("Naveen Balani");
+//			article.setLevel("Intermediate");
+//			article.setDate("January-2004");
+//			articleList.add(article);
+//
+//			article = factory.createArticleType();
+//			article.setTitle("Advance DAO Programming");
+//			article.setAuthor("Sean Sullivan");
+//			article.setLevel("Advanced");
+//			article.setDate("October-2003");
+//			articleList.add(article);
+//
+//			article = factory.createArticleType();
+//			article.setTitle("Best Practices in EJB Exception Handling");
+//			article.setAuthor("Srikanth Shenoy");
+//			article.setLevel("Advanced");
+//			article.setDate("May-2002");
+//			articleList.add(article);
+//
+//			JAXBElement<CatalogType> catalogElement =
+//                    factory.createCatalog(catalog);
+//			marshaller.setProperty("jaxb.formatted.output", Boolean.TRUE);
+//			marshaller.marshal(catalogElement, System.out);
 		}
         catch (JAXBException ex) {
 			ex.printStackTrace();
